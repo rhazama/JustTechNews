acb5628d1c85d62fc88bfd377e25d1624a4eedde
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-02 12:25:22 -0800

    created post route for user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,26 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        res.json({ user: dbUserData });
+    
+        // Verify user
+      // found in http://localhost:3001/api/users/login
+
+      });  
+    });
+
 // create our User model
 class User extends Model {}
 
