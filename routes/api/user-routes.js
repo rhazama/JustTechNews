2d81e8f62e2aa7012bb759be09af122d4a259b64
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-02 13:06:30 -0800

    add password routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,23 +3,25 @@ const sequelize = require('../config/connection');
 
 router.post('/login', (req, res) => {
     // expects {email: 'lernantino@gmail.com', password: 'password1234'}
-      User.findOne({
-        where: {
-          email: req.body.email
-        }
-      }).then(dbUserData => {
-        if (!dbUserData) {
-          res.status(400).json({ message: 'No user with that email address!' });
-          return;
-        }
-    
-        res.json({ user: dbUserData });
-    
-        // Verify user
-      // found in http://localhost:3001/api/users/login
-
-      });  
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
     });
+  });
 
 // create our User model
 class User extends Model {}
