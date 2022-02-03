3e5f71ebab91207a942ec6f144294889b227da0c
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-02 18:22:48 -0800

    API Post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require("./User");
-const Post = require("./Post")
+const Post = require("./Post");
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+module.exports = router;
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
