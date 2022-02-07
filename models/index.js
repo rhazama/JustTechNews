cc4105c07807466e1d45fa60aad09ca6e1fc2af8
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-06 16:31:21 -0800

    added routers

--- models/index.js
+++ models/index.js
@@ -5,13 +5,18 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 
 module.exports = router;
-
-Post.belongsTo(User, {
-    foreignKey: 'user_id',
-  });
 
 module.exports = { User, Post };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -19,3 +19,5 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
         });
   });
+  
+  module.exports = router;
