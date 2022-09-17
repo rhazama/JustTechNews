7c0a20751bbdef31b6c27f1ad438ce0f7ff0af52
rhazama <rhazama01@gmail.com>
2022-09-17 16:31:20 -0700

    Adding sequelize

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
+  
   Post.findAll({
     attributes: [
       'id', 
