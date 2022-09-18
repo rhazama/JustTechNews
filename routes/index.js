279ba48338b682e52c6626007ac287ca12824d8b
rhazama <rhazama01@gmail.com>
2022-09-17 18:40:14 -0700

    big fixes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const apiRoutes = require("./api").default;
+const apiRoutes = require('./api');
 
-router.use("/api", apiRoutes);
+router.use('/api', apiRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
