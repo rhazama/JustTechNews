529285c8b176323864b4e1f9d689d1082789fae9
rhazama <rhazama01@gmail.com>
2022-10-03 09:46:57 -0700

    adding user id and username to user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,11 +60,15 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json(dbUserData);
     });
+  })
 });
 
 router.post('/login', (req, res) => {
