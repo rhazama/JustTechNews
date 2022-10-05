74557f5dce64f737248266e3a7ff4f56e56c2760
rhazama <rhazama01@gmail.com>
2022-10-05 13:00:45 -0700

    adjusting url format

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -5,3 +5,13 @@ test('format_date() returns a date string', () => {
   
     expect(format_date(date)).toBe('3/20/2020');
   });
+
+  test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
