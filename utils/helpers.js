52cc674230d2d597a73b0b653a98c5248dabd4ae
rhazama <rhazama01@gmail.com>
2022-10-05 13:06:51 -0700

    Adding other statements for url format

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,6 +12,10 @@ module.exports = {
 
     format_url: url => {
         return url
-          .replace('http://', '')
+            .replace('http://', '')
+            .replace('https://', '')
+            .replace('www.', '')
+            .split('/')[0]
+            .split('?')[0];
       },
   }
