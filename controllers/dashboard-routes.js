401762ce95be4ddd5e1460516b50723c07713875
rhazama <rhazama01@gmail.com>
2022-10-06 16:11:22 -0700

    adjusting get route for edit/id

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,11 +43,7 @@ router.get('/', withAuth, (req, res) => {
   });
 
   router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
-      where: {
-        // use the ID from the session
-        user_id: req.session.user_id
-      },
+    Post.findByPk(req.params.id, {
       attributes: [
         'id',
         'post_url',
@@ -70,7 +66,21 @@ router.get('/', withAuth, (req, res) => {
         }
       ]
     })
-
-  }); 
+      .then(dbPostData => {
+        if (dbPostData) {
+          const post = dbPostData.get({ plain: true });
+          
+          res.render('edit-post', {
+            post,
+            loggedIn: true
+          });
+        } else {
+          res.status(404).end();
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
