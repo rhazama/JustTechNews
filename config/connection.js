14c3ade4f83462f2763241656787ba36a808758d
rhazama <rhazama01@gmail.com>
2022-10-08 20:44:17 -0700

    adjusting sequelize route

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,8 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      user: 'root',
+      password: 'HeyJessie',
       port: 3306
     });
 
