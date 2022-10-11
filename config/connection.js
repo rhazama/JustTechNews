05d3d0147baa0bee93ae4ad363efbd279309730d
rhazama <rhazama01@gmail.com>
2022-10-10 21:40:38 -0700

    adjusting password

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,9 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);, {
       host: 'localhost',
       dialect: 'mysql',
-      user: 'root',
-      password: 'HeyJessie',
       port: 3306
     });
 
